49ef90351d85548a963d695cad155ca3b08f8744
kihooncode <kihooncode@gmail.com>
2021-01-21 23:44:00 +0900

    indented all js files

--- App.js
+++ App.js
@@ -2,71 +2,71 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
-  // user's goal
-  const [inputGoal, setInputGoal] = useState("");
-  // user's goal order
-  const [count, setCount] = useState(1);
-  // list of goals of user
-  const [goalList, setGoalList] = useState([]);
+   // user's goal
+   const [inputGoal, setInputGoal] = useState("");
+   // user's goal order
+   const [count, setCount] = useState(1);
+   // list of goals of user
+   const [goalList, setGoalList] = useState([]);
 
-  const inputGoalHandler = input => {
-    setInputGoal(input);
-  } 
+   const inputGoalHandler = input => {
+      setInputGoal(input);
+   }
 
-  const addGoal = () => {
-    setGoalList([...goalList, {id: Math.random().toString() , value: count + ". " + inputGoal}]);
-    setCount(count + 1);
-    // difference between above and below code
-    //setGoalList(curr => [...curr, inputGoal]);
-  }
+   const addGoal = () => {
+      setGoalList([...goalList, { id: Math.random().toString(), value: count + ". " + inputGoal }]);
+      setCount(count + 1);
+      // difference between above and below code
+      //setGoalList(curr => [...curr, inputGoal]);
+   }
 
-  return (
-    <View style={styles.screen}>
-      <View style={styles.input_container}>
-        <TextInput 
-          placeholder="Your goal"  
-          style={styles.input}
-          onChangeText={inputGoalHandler} 
-          value={inputGoal}  
-        />
-        <Button title="ADD"
-          onPress={addGoal}       
-        />
+   return (
+      <View style={styles.screen}>
+         <View style={styles.input_container}>
+            <TextInput
+               placeholder="Your goal"
+               style={styles.input}
+               onChangeText={inputGoalHandler}
+               value={inputGoal}
+            />
+            <Button title="ADD"
+               onPress={addGoal}
+            />
+         </View>
+         <FlatList
+            data={goalList}
+            renderItem={itemData =>
+               <View style={styles.goalList}>
+                  <Text>{itemData.item.value}</Text>
+               </View>}>
+         </FlatList>
       </View>
-      <FlatList 
-        data={goalList} 
-        renderItem={itemData => 
-                    <View style={styles.goalList}>
-                      <Text>{itemData.item.value}</Text>
-                    </View>}>
-      </FlatList>
-    </View>
-  );
+   );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    padding: 50
-  },
+   screen: {
+      padding: 50
+   },
 
-  input_container: {
-    flexDirection: 'row', 
-    alignItems: 'center', 
-    justifyContent: 'space-between'
-  },
-  
-  input: {
-    width: '87%',
-    borderColor: "black", 
-    borderWidth: 1, 
-    padding: 10
-  },
+   input_container: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between'
+   },
 
-  goalList: {
-    borderWidth: 1,
-    marginVertical: 7,
-    padding: 10,
-    width: '87%',
-    backgroundColor: '#99FFFF'
-  }
+   input: {
+      width: '87%',
+      borderColor: "black",
+      borderWidth: 1,
+      padding: 10
+   },
+
+   goalList: {
+      borderWidth: 1,
+      marginVertical: 7,
+      padding: 10,
+      width: '87%',
+      backgroundColor: '#99FFFF'
+   }
 });
--- components/GoalsInput.js
+++ components/GoalsInput.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import {} from 'react-native';
+import { } from 'react-native';
 
 const GoalsInput = props => {
-    return
+   return (
+      <View style={styles.input_container}>
+         <TextInput
+            placeholder="Your goal"
+            style={styles.input}
+            onChangeText={inputGoalHandler}
+            value={inputGoal}
+         />
+         <Button title="ADD"
+            onPress={addGoal}
+         />
+      </View>
+   );
 };
 
 export default GoalsInput;
